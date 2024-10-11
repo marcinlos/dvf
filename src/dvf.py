a734813250671cee1bc8ec92999e61778bf2c915
Marcin Łoś <marcin.los.91@gmail.com>
2024-10-11 22:21:25 +0200

    Add function removing DoFs from arrays

--- src/dvf.py
+++ src/dvf.py
@@ -587,6 +587,44 @@ def select_dofs(space, condition, invert=False):
     return np.array(list(gen()), dtype=np.intp)
 
 
+def remove_dofs(a, dofs=None, *, trial_dofs=None, test_dofs=None):
+    match a.ndim:
+        case 1:
+            if trial_dofs is not None:
+                raise ValueError("Cannot specify trial DoFs for vector")
+
+            if test_dofs is not None:
+                raise ValueError("Cannot specify test DoFs for vector")
+
+            if dofs is None:
+                raise ValueError("DoFs to remove not specified")
+
+            return np.delete(a, dofs)
+
+        case 2:
+            if dofs is not None:
+                if trial_dofs is not None:
+                    raise ValueError("Cannot mix `dofs` and `trial_dofs`")
+
+                if test_dofs is not None:
+                    raise ValueError("Cannot mix `dofs` and `test_dofs`")
+
+                trial_dofs_ = dofs
+                test_dofs_ = dofs
+            else:
+                if trial_dofs is None and test_dofs is None:
+                    raise ValueError("DoFs to remove not specified")
+
+                trial_dofs_ = trial_dofs or ()
+                test_dofs_ = test_dofs or ()
+
+            b = np.delete(a, test_dofs_, axis=0)
+            return np.delete(b, trial_dofs_, axis=1)
+
+        case _:
+            raise ValueError(f"Tensors of rank {a.ndim} are not supported")
+
+
 def _relevant_points(p, grid):
     # assuming first-order differential operators, it is enough to consider the
     # cross-shaped stenicil
---
+++ tests/test_bc.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pytest
+
+from dvf import remove_dofs
+
+
+@pytest.fixture
+def mat():
+    return np.arange(16).reshape(4, 4)
+
+
+def test_can_remove_dofs_from_vector():
+    vec = np.arange(10)
+    dofs = [1, 4, 5, 8]
+    reduced = remove_dofs(vec, dofs)
+
+    np.testing.assert_allclose(reduced, [0, 2, 3, 6, 7, 9])
+
+
+def test_cannot_remove_test_dofs_from_vector():
+    vec = np.arange(10)
+    with pytest.raises(ValueError, match="test DoFs"):
+        remove_dofs(vec, test_dofs=[1])
+
+
+def test_cannot_remove_trial_dofs_from_vector():
+    vec = np.arange(10)
+    with pytest.raises(ValueError, match="trial DoFs"):
+        remove_dofs(vec, trial_dofs=[1])
+
+
+def test_must_specify_dofs_to_remove_from_vector():
+    vec = np.arange(10)
+    with pytest.raises(ValueError, match="not specified"):
+        remove_dofs(vec)
+
+
+def test_can_remove_dofs_from_matrix(mat):
+    dofs = [0, 2]
+
+    reduced = remove_dofs(mat, dofs)
+
+    expected = np.array([[5, 7], [13, 15]])
+    np.testing.assert_allclose(reduced, expected)
+
+
+def test_can_remove_trial_dofs_from_matrix(mat):
+    dofs = [0, 2]
+
+    reduced = remove_dofs(mat, trial_dofs=dofs)
+
+    expected = np.array([[1, 3], [5, 7], [9, 11], [13, 15]])
+    np.testing.assert_allclose(reduced, expected)
+
+
+def test_can_remove_test_dofs_from_matrix(mat):
+    dofs = [0, 2]
+
+    reduced = remove_dofs(mat, test_dofs=dofs)
+
+    expected = np.array([[4, 5, 6, 7], [12, 13, 14, 15]])
+    np.testing.assert_allclose(reduced, expected)
+
+
+def test_can_remove_trial_and_test_dofs_from_matrix(mat):
+    trial_dofs = [0, 2]
+    test_dofs = [1, 3]
+
+    reduced = remove_dofs(mat, trial_dofs=trial_dofs, test_dofs=test_dofs)
+
+    expected = np.array([[1, 3], [9, 11]])
+    np.testing.assert_allclose(reduced, expected)
+
+
+def test_cannot_remove_dofs_and_trial_dofs_from_matrix(mat):
+    with pytest.raises(ValueError, match="trial_dofs"):
+        remove_dofs(mat, [0], trial_dofs=[0])
+
+
+def test_cannot_remove_dofs_and_test_dofs_from_matrix(mat):
+    with pytest.raises(ValueError, match="test_dofs"):
+        remove_dofs(mat, [0], test_dofs=[0])
+
+
+def test_must_specify_some_dofs_to_remove_from_matrix(mat):
+    with pytest.raises(ValueError, match="not specified"):
+        remove_dofs(mat)
+
+
+def test_cannot_remove_from_higher_order_tensors():
+    vec = np.zeros((2, 2, 2))
+    with pytest.raises(ValueError, match="rank 3"):
+        remove_dofs(vec, [])
